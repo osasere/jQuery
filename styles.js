52103d9eebfde9dfea1ad6862106d6c5c27c4435
Osas <osas.mike@hotmail.com>
2020-02-20 12:32:29 -0500

    Initial commit

---
+++ styles.js
@@ -0,0 +1,16 @@
+var times = [
+    "9am",
+    "10am",
+    "11am",
+    "12pm",
+    "1pm",
+    "2pm",
+    "3pm",
+    "4pm",
+    "5pm",
+]
+
+for (var i = 0; i < times.length; i++) {
+    
+    
+}
